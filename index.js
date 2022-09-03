39688dbe781066058b53e0b33d67b6ca47cf0c64
ZhafranT <zhaftosa@gmail.com>
2022-09-03 12:15:12 +0700

    app hotel with mern stack

---
+++ index.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import authRoute from './routes/auth.js';
+import hotelsRoute from './routes/hotels.js';
+import roomsRoute from './routes/rooms.js';
+import usersRoute from './routes/users.js';
+import cookieParser from 'cookie-parser';
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+   try {
+      await mongoose.connect(process.env.MONGO);
+      console.log('Connected to mongoDB.');
+   } catch (error) {
+      throw error;
+   }
+};
+
+mongoose.connection.on('disconnect', () => {
+   console.log('Disconnected from mongoDB.');
+});
+
+// middlewares
+app.use(cookieParser());
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/hotels', hotelsRoute);
+app.use('/api/rooms', roomsRoute);
+
+app.use((req, res, next) => {
+   res.send('hello from middleware');
+   // console.log("hi iam a middleware!");
+});
+
+app.listen(8800, () => {
+   connect();
+   console.log('Connected to backend.');
+});
+
+// time in video 1:00:44
