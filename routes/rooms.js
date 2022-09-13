14c5de88ec1086d36b8b23c7cf87faf81fe6ca40
ZhafranT <zhaftosa@gmail.com>
2022-09-13 19:21:44 +0700

    backend final

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/room.js';
+import { verifyisAdmin } from '../utlis/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-   res.send('Hellow, this is rooms endpoint');
-});
+// CREATE
+router.post('/:hotelid', verifyisAdmin, createRoom);
+
+// UPDATE
+router.put('/:id', verifyisAdmin, updateRoom);
+
+// DELETE
+router.delete('/:id/:hotelid', verifyisAdmin, deleteRoom);
+
+// GET
+router.get('/:id', getRoom);
+
+// GET ALL
+router.get('/', getRooms);
 
 export default router;
